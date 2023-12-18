eb3334a2ae88f83d0904dea7fb8d860bee109e5c
ganeshdatta23 <90389039+ganeshdatta23@users.noreply.github.com>
2023-12-18 15:48:46 +0530

    human-counting-project-code.py

--- human-counting-project-code.py
+++ human-counting-project-code.py
@@ -3,50 +3,54 @@ import imutils
 import numpy as np
 import argparse
 
+# Function to detect humans in a frame using the HOG descriptor
 def detect(frame):
-    bounding_box_cordinates, weights =  HOGCV.detectMultiScale(frame, winStride = (4, 4), padding = (8, 8), scale = 1.03)
+    bounding_box_cordinates, weights = HOGCV.detectMultiScale(frame, winStride=(4, 4), padding=(8, 8), scale=1.03)
     
     person = 1
-    for x,y,w,h in bounding_box_cordinates:
-        cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
-        cv2.putText(frame, f'person {person}', (x,y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1)
+    for x, y, w, h in bounding_box_cordinates:
+        # Draw rectangle around detected person
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        # Add person label
+        cv2.putText(frame, f'person {person}', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
         person += 1
     
-    cv2.putText(frame, 'Status : Detecting ', (40,40), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255,0,0), 2)
-    cv2.putText(frame, f'Total Persons : {person-1}', (40,70), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255,0,0), 2)
+    # Display status and count of detected persons
+    cv2.putText(frame, 'Status : Detecting ', (40, 40), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 0, 0), 2)
+    cv2.putText(frame, f'Total Persons : {person - 1}', (40, 70), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 0, 0), 2)
     cv2.imshow('output', frame)
 
     return frame
 
+# Function to process video file frame by frame and perform human detection
 def detectByPathVideo(path, writer):
-
     video = cv2.VideoCapture(path)
     check, frame = video.read()
-    if check == False:
+    if check is False:
         print('Video Not Found. Please Enter a Valid Path (Full path of Video Should be Provided).')
         return
 
     print('Detecting people...')
     while video.isOpened():
-        #check is True if reading was successful 
-        check, frame =  video.read()
+        check, frame = video.read()
 
         if check:
-            frame = imutils.resize(frame , width=min(800,frame.shape[1]))
+            frame = imutils.resize(frame, width=min(800, frame.shape[1]))
             frame = detect(frame)
             
             if writer is not None:
                 writer.write(frame)
             
             key = cv2.waitKey(1)
-            if key== ord('q'):
+            if key == ord('q'):
                 break
         else:
             break
     video.release()
     cv2.destroyAllWindows()
 
-def detectByCamera(writer):   
+# Function to activate the camera and perform human detection on the live feed
+def detectByCamera(writer):
     video = cv2.VideoCapture(0)
     print('Detecting people...')
 
@@ -59,15 +63,16 @@ def detectByCamera(writer):
 
         key = cv2.waitKey(1)
         if key == ord('q'):
-                break
+            break
 
     video.release()
     cv2.destroyAllWindows()
 
+# Function to detect humans in a single image file
 def detectByPathImage(path, output_path):
     image = cv2.imread(path)
 
-    image = imutils.resize(image, width = min(800, image.shape[1])) 
+    image = imutils.resize(image, width=min(800, image.shape[1]))
 
     result_image = detect(image)
 
@@ -76,19 +81,23 @@ def detectByPathImage(path, output_path):
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+# Main function to determine the input source and initiate detection
 def humanDetector(args):
     image_path = args["image"]
     video_path = args['video']
-    if str(args["camera"]) == 'true' : camera = True 
-    else : camera = False
+    if str(args["camera"]) == 'true':
+        camera = True
+    else:
+        camera = False
 
     writer = None
     if args['output'] is not None and image_path is None:
-        writer = cv2.VideoWriter(args['output'],cv2.VideoWriter_fourcc(*'MJPG'), 10, (600,600))
+        writer = cv2.VideoWriter(args['output'], cv2.VideoWriter_fourcc(*'MJPG'), 10, (600, 600))
 
     if camera:
         print('[INFO] Opening Web Cam.')
-        detectByCamera(ouput_path,writer)
+        detectByCamera(writer)
     elif video_path is not None:
         print('[INFO] Opening Video from path.')
         detectByPathVideo(video_path, writer)
@@ -96,6 +105,7 @@ def humanDetector(args):
         print('[INFO] Opening Image from path.')
         detectByPathImage(image_path, args['output'])
 
+# Function to parse command-line arguments
 def argsParser():
     arg_parse = argparse.ArgumentParser()
     arg_parse.add_argument("-v", "--video", default=None, help="path to Video File ")
@@ -107,9 +117,11 @@ def argsParser():
     return args
 
 if __name__ == "__main__":
+    # Initialize the HOG descriptor for person detection
     HOGCV = cv2.HOGDescriptor()
     HOGCV.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
+    # Parse command-line arguments
     args = argsParser()
+    # Start human detection based on the provided arguments
     humanDetector(args)
-
